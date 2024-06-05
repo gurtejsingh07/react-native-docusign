287ec4d0310ca04e0253279ab9ab131cf5e0e274
Gurtej <gurtej@Gurtejs-MacBook-Pro.local>
2024-06-05 23:27:32 +0530

    added navigation lib support

---
+++ src/container/profile/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
+  export default ProfileScreen
