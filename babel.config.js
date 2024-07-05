c05fafd4db38b3c76385ebba3da7a2cbaedf3fd4
gurtejsmartdata <38884969+gurtejsmartdata@users.noreply.github.com>
2024-07-05 19:09:18 +0530

    add module-resolver support

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    "react-native-reanimated/plugin",
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@routes': './src/routes',
+          '@utils': './src/utils',
+          '@assets':'./src/assets',
+          '@config':'./src/config'
+          // Add more aliases as needed
+        },
+      },
+    ],
   ],
 };
